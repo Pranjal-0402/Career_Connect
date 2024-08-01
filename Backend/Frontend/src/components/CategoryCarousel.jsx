bce61aaff2c4bb2391a6834cdda2061c9f6ff174
Pranjal Mishra <114647342+Pranjal-0402@users.noreply.github.com>
2024-08-01 11:11:58 +0530

    Update CategoryCarousel.jsx

--- Backend/Frontend/src/components/CategoryCarousel.jsx
+++ Backend/Frontend/src/components/CategoryCarousel.jsx
@@ -27,7 +27,7 @@ export function CategoryCarousel() {
     return (
         <Carousel className="w-full max-w-xl mx-auto my-10">
             <CarouselContent>
-                {category.map((item, index) => (
+                {category?.map((item, index) => (
                     <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                         <div className="p-1">
                             <Button onClick={() => {
