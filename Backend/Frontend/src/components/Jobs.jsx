baba6ff15e1d0ea2bbf16766c5c69d51656ef80d
Pranjal Mishra <114647342+Pranjal-0402@users.noreply.github.com>
2024-08-01 11:49:55 +0530

    Update Jobs.jsx

--- Backend/Frontend/src/components/Jobs.jsx
+++ Backend/Frontend/src/components/Jobs.jsx
@@ -42,13 +42,13 @@ const Jobs = () => {
                         <FilterCard />
                     </div>
                     {
-                        filterJobs.length <= 0 ? <Jobnotfound /> : (
+                        filterJobs?.length <= 0 ? <Jobnotfound /> : (
                             <div className='flex-1 h-[88vh] overflow-y-auto  no-scrollbar pb-5'>
                                 <div className='grid grid-cols-3 gap-4'>
                                     {
                                         filterJobs && filterJobs?.map((job) => (
                                             <motion.div
-                                                key={job._id}
+                                                key={job?._id}
                                                 initial={{ opacity: 0, x: 100 }}
                                                 animate={{ opacity: 1, x: 0 }}
                                                 exit={{ opacity: 0, x: -100 }}
